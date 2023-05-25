972130560eb7ecec7d92f4827393fb7a9fe3e7cd
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-05-25 10:51:34 -0300

    change info

--- cpt-events.php
+++ cpt-events.php
@@ -1,5 +1,16 @@
 <?php
-
+/*
+Plugin Name:  CPT Eventos
+Plugin URI:   https://github.com/lucassdantas/cpt-eventos
+Description:  Plugin para adicionar CPT para eventos do Mirante
+Version:      1.0
+Author:       Lucas Dantas 
+Author URI:   https://github.com/lucassdantas
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  cpt-eventos
+Domain Path:  /languages
+*/
 
 
 ?>
